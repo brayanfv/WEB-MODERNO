47f48968720f79f08b72ea9d8c2fe5f62bcdb407
Brayan Favarin <brayanmf1227@gmail.com>
2025-02-04 12:36:07 -0300

    Add files via upload

---
+++ TESTE CURSO/CURSO WEB MODERNO/INICIO/dados_1.js
@@ -0,0 +1,14 @@
+let nome = "Caneta";
+let quantidade = 10;  // variaveis com valores definidos
+let preco = 6.4;
+let imposto = 1.5;                       // utilize var ou let para definir uma variavel
+let precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "Caneta BIC";
+console.log(nome);
---
+++ TESTE CURSO/CURSO WEB MODERNO/INICIO/desafio_area.js
@@ -0,0 +1,10 @@
+const PI = 3.14;
+const raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor daarea e " + areaCirc + " m2.");
+
+areaCirc = Math.PI * raio * raio;
+console.log("O valor daarea e " + areaCirc + " m2.");
+
+
