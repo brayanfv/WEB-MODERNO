a4f5a7f76575089d2ff8ab5e4f9ee15050b42405
brayanfv <brayanmf1227@gmail.com>
2025-02-04 16:56:23 -0300

    forEach

---
+++ TESTE CURSO/Exercicios-js/Array/forEach1.js
@@ -0,0 +1,10 @@
+const aprovados = ['Agatha', 'Aldo', 'Daniel', 'Raquel']
+
+aprovados.forEach(function(nome, indice) {  //forEach = 3 parametros ( nome, indice e ARRAY)
+    console.log(`${indice = 1}) ${nome}`)
+})
+
+aprovados.forEach(nome => console.log(nome))
+
+const exibirAprovados= aprovados => console.log (aprovado)
+aprovado.forEach(exibirAprovados)
